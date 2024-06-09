396ee7d19a06acaa6d248d4885bed6369db1ce78
w45242hy <hin.yip-2@student.manchester.ac.uk>
2024-06-09 15:00:38 +0100

    feat: create hook useKeyPress()

---
+++ client/app/lib/hooks.ts
@@ -0,0 +1,34 @@
+/*
+Reference link: https://www.youtube.com/watch?v=Aq8PnD-gn1g
+Name of video: Learn how to call a function when pressing a specific key | Custom React Hook | useOnKeyPress
+*/
+import React, { useEffect } from "react";
+
+/**
+ * Does not work
+ * @param callback 
+ * @param targetKey 
+ */
+// export const useKeyDown = (callback: Function, targetKey: string) => {
+//     useEffect(() => {
+//         const keyPressHandler = (event: KeyboardEvent) => {
+//             if(event.key === targetKey) {
+//                 callback();
+//             }
+//         };
+//         window.addEventListener("keydown", keyPressHandler);
+//         return () => {
+//             window.removeEventListener("keydown", keyPressHandler);
+//         };
+//     }, [callback, targetKey]);
+// };
+
+export const useKeyPress = (handler: EventListenerOrEventListenerObject, dependencies = []) => {
+    useEffect(() => {
+        document.addEventListener("keydown", handler);
+        // clean up
+        return () => {
+            document.removeEventListener("keydown", handler);
+        };
+    }, dependencies);
+};
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+import { useState } from "react";
 import Obstacle from "./components/obstacle";
 import Player from "./components/sprite/player";
+import { useKeyDown, useKeyPress } from "./lib/hooks";
 
 export default function App() {
+
+  const [pressedKey, setKey] = useState("Initial key");
+  const pressKey = (keyCode: string) => {
+    setKey(keyCode);
+  };
+
+  // Demo
+  // useKeyPress((e: KeyboardEvent) => {console.log(`event e = ${e.code}`);setKey(e.code)}, []);    // Ignore the red line. VS code is tripping
+  useKeyPress((e: KeyboardEvent) => {pressKey(e.code)}, []);    // Ignore the red line. VS code is tripping
+
+
   return (
+    // tabIndex defins the order in which the elements are focused when using keyboard navigation
     <div>
       <h1>Hello World123</h1>
+      <p>Key pressed: {pressedKey}</p>
       <Player x={25} y={25} width={50} height={50} color="#00416d" />
       <Obstacle x={15} y={25} width={50} height={50} color="green" />
     </div>

0e7ee22486fed4f15b376b227a1d61fb6057f262
w45242hy <hin.yip-2@student.manchester.ac.uk>
2024-06-09 11:10:25 +0100

    chore: update file

--- client/app/lib/definitions.ts
+++ client/app/lib/definitions.ts
@@ -7,10 +7,18 @@ enum Direction {
 }
 
 type Player = {
-    x: number, 
-    y: number, 
+    x: number,    // The x-coordinate of the centre of the block
+    y: number,    // The y-coordinate of the centre of the block
     width: number, 
     height: number, 
     color: string, 
     direction: Direction
+}
+
+type Obstacle = {
+    x: number,    // The x-coordinate of the centre of the block
+    y: number,    // The y-coordinate of the centre of the block
+    width: number, 
+    height: number,    
+    color: string
 }
